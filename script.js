ddd83b13932630d091865cc489a8ac23d5cad8ba
Tonmoy <tonmoybiswas20602@gmail.com>
2025-08-29 18:16:15 +0800

    js uodated

--- script.js
+++ script.js
@@ -15,3 +15,16 @@ let coins = parseInt(coinCount.innerText);
 const callHistoryList = document.getElementById("callHistoryList");
 
 const callButtons = document.querySelectorAll(".callBtn");
+//commit korte hbe
+
+for (let i = 0; i < callButtons.length; i++) {
+  callButtons[i].addEventListener("click", function () {
+    let card = callButtons[i].parentNode;
+    while (card && !card.classList.contains("bg-white")) {
+      card = card.parentNode;
+    }
+
+    const serviceName = card.querySelector(".service-name").innerText;
+    const serviceNumber = card.querySelector(".phn-num").innerText;
+
+    
